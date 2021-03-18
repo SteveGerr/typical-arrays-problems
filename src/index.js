0c05cbbdb9ff3d2218e0eb00fe0ffcf63460dcf0
Ivan <shunyaeviv@63pokupki.ru>
2021-03-18 10:48:02 +0400

    Passing typical arrays

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
+    if (arguments.length !== 0 && array.length !== 0) {
+        return Math.min.apply(null, array)
+    }
   return 0;
 }
 
 exports.max = function max (array) {
+    if (arguments.length !== 0 && array.length !== 0) {
+        return Math.max.apply(null, array)
+    }
+
   return 0;
 }
 
 exports.avg = function avg (array) {
+    if (arguments.length !== 0 && array.length !== 0) {
+        // Получаем сумму всех чисел и делим на количество этих чисел
+        return array.reduce((a, b) => (a + b)) / array.length;
+    }
+
   return 0;
 }
